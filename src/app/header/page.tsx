2dcb2f3b2661b36820f900a9b6eb28659f76f31d
Reyhan Rumengan <reyhanrumengan98@gmail.com>
2023-10-25 23:03:42 +0200

    finish home

--- src/app/header/page.tsx
+++ src/app/header/page.tsx
@@ -6,28 +6,12 @@ function Header() {
     <>
       <div className={styles.header}>
         <Image
-          width={204.5}
-          height={40}
+          width={280}
+          height={55}
           className={styles.headerIcon}
           alt="European Reformed Evangelical Convention 2024"
           src="/logo.svg"
         />
-        <div className={styles.stamp}>
-          <Image
-            width={180}
-            height={180}
-            className={styles.stampFrame}
-            alt="Open Registration"
-            src="/stamp-frame.svg"
-          />
-          <Image
-            width={180}
-            height={180}
-            className={styles.stampContent}
-            alt="Nov 1"
-            src="/stamp-content.svg"
-          />
-        </div>
       </div>
     </>
   );
