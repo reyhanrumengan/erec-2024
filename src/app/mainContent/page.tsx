5802d31c3c1f97ae7b1001a3075ffcb59acda87c
Reyhan Rumengan <reyhanrumengan98@gmail.com>
2023-10-30 23:09:39 +0100

    update link for registration

--- src/app/mainContent/page.tsx
+++ src/app/mainContent/page.tsx
@@ -17,7 +17,7 @@ function MainContent() {
           />
           <div className={styles.buttonContainer}>
             <a
-              href="https://www.jugendherberge.de/en/youth-hostels/halle/"
+              href="https://docs.google.com/forms/d/e/1FAIpQLSc-ionffNyShdgT-3PaW2UAMVD7PZiuUzXhXi7sEu2KwkP63Q/viewform"
               target="_blank"
               rel="noopener noreferrer"
               className={styles.button}
