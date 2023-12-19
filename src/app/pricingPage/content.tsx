274cb804506e5bff328d43f704dcf7eadf98e194
Reyhan Rumengan <reyhanrumengan98@gmail.com>
2023-12-19 19:24:41 +0100

    add new files for pricing page

---
+++ src/app/pricingPage/content.tsx
@@ -0,0 +1,73 @@
+import styles from "./themePage.module.css";
+import Image from "next/image";
+
+export default function Content() {
+  return (
+    <>
+      <div className={styles.heroContainer}>
+        <div className={styles.container1}>
+          <div>
+            <p className={styles.textHeader}>ASSURANCE OF GRACE</p>
+            <p className={styles.smallText}>
+              Read an explanation directly from IREC Europe&rsquo;s lead pastor
+              as well as one of our speakers on why this theme.
+            </p>
+          </div>
+          <div>
+            {/* <p
+              style={{
+                fontSize: "300px",
+                fontFamily: "Dynalight",
+                color: "#fff",
+                position: "absolute",
+                left: "-20px", // Adjust the left value as needed
+                top: "-50px", // Adjust the top value as needed
+                margin: "0",
+              }}
+            >
+              “”
+            </p> */}
+            <p className={styles.text} style={{ width: "425px" }}>
+              In Evangelical theology we are usually taught to bring someone to
+              unwavering assurance of salvation once he or she believes. Without
+              this assurance, the authenticity of one&rsquo;s faith should be
+              doubted.
+            </p>
+          </div>
+        </div>
+        <div className={styles.container2}>
+          <p
+            className={styles.text}
+            style={{ textAlign: "center", width: "650px" }}
+          >
+            Is this kind of concept right? <br />
+            How do the Reformed tradition and the witness of the Scripture
+            actually teach about this assurance of salvation?
+          </p>
+
+          <div className={styles.linkToSpeakerContainer}>
+            <p className={styles.linkToSpeaker}>
+              Hear to what our Speakers say
+            </p>
+
+            {/* change link later to redirect to Speaker Page */}
+            <a
+              href="https://www.jugendherberge.de/en/youth-hostels/halle/"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ height: "28px" }}
+            >
+              <Image
+                width={28}
+                height={28}
+                className={styles.arrowCircleRightIcon}
+                alt="Arrow Circle Right"
+                src="/arrowCircleRight.svg"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
