456d4b45cb89c40460bbd71a7e7a739dac11466a
Reyhan Rumengan <reyhanrumengan98@gmail.com>
2023-12-20 13:27:47 +0100

    finish homepage mobile

--- src/app/homePage/page.tsx
+++ src/app/homePage/page.tsx
@@ -16,7 +16,7 @@ export default function HomePage() {
           <Content />
         </div>
 
-        <MenuSidebar />
+        {/* <MenuSidebar /> */}
       </div>
 
       <Footer />
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ColorSchemeScript, MantineProvider } from "@mantine/core";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,7 +28,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      {/* <body className={inter.className}>{children}</body> */}
+      <head>
+        <ColorSchemeScript />
+      </head>
+      <body>
+        <MantineProvider>{children}</MantineProvider>
+      </body>
     </html>
   );
 }
