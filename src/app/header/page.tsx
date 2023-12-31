4a28e40017c9caa2c57f44ceb2e58c1f21aa73aa
Reyhan Rumengan <reyhanrumengan98@gmail.com>
2023-12-31 10:41:24 +0100

    test

--- src/app/header/page.tsx
+++ src/app/header/page.tsx
@@ -1,20 +1,26 @@
 import Image from "next/image";
 import styles from "./header.module.css";
 import Link from "next/link";
+import MenuBurger from "../menuBurger/page";
 
 export default function Header() {
   return (
     <>
       <div className={styles.header}>
-        <Link href="/">
-          <Image
-            width={280}
-            height={55}
-            className={styles.headerIcon}
-            alt="European Reformed Evangelical Convention 2024"
-            src="/logo.svg"
-          />
-        </Link>
+        <div className={styles.headerContainer}>
+          <Link href="/">
+            <Image
+              width={280}
+              height={55}
+              className={styles.headerIcon}
+              alt="European Reformed Evangelical Convention 2024"
+              src="/logo.svg"
+            />
+          </Link>
+        </div>
+        <div className={styles.burgerContainer}>
+          <MenuBurger />
+        </div>
       </div>
     </>
   );
--- src/app/menuBurger/page.tsx
+++ src/app/menuBurger/page.tsx
@@ -1,26 +1,110 @@
+"use client";
 import Link from "next/link";
 import styles from "./menuBurger.module.css";
+import { useDisclosure } from "@mantine/hooks";
+import { Menu, Burger, Button } from "@mantine/core";
+import { useState } from "react";
 
 export default function MenuBurger() {
+  const [burgerOpened, { toggle }] = useDisclosure();
+  const [menuOpened, setMenuOpened] = useState(false);
+
+  const handleMenuClick = () => {
+    setMenuOpened(!menuOpened);
+    toggle();
+  };
+
   return (
     <>
-      {/* <div className={styles.menuBurger}>
-        <Link className={styles.menuText} href="/theme">
-          THEME
-        </Link>
-        <p className={styles.menuText}>SPEAKERS</p>
-        <p className={styles.menuText}>EREC</p>
-        <p className={styles.menuText}>VENUE</p>
-        <Link className={styles.menuText} href="/fee">
-          FEE
-        </Link>
-        <Link className={styles.menuText} href="/faqs">
-          FAQS
-        </Link>
-        <Link className={styles.menuText} href="/contact">
-          CONTACT
-        </Link>
-      </div> */}
+      {/* {menuOpened && (
+        <div
+          className={styles.overlay}
+          onClick={() => {
+            setMenuOpened(false);
+            toggle();
+          }}
+        ></div>
+      )} */}
+      <Menu
+        opened={menuOpened}
+        onChange={setMenuOpened}
+        classNames={{
+          dropdown: styles.dropdown,
+          item: styles.item,
+        }}
+      >
+        <Menu.Target>
+          {/* <Button> */}
+          <Burger
+            opened={burgerOpened}
+            onClick={handleMenuClick}
+            aria-label="Toggle navigation"
+          />
+          {/* </Button> */}
+        </Menu.Target>
+
+        <Menu.Dropdown>
+          <div className={styles.dropdownContainer}>
+            <Menu.Item>
+              <div className={styles.buttonContainer}>
+                <a
+                  // href="https://link.grii.eu/EREC2024"
+                  href="https://forms.gle/5nBNSFBUs6AUr8bz6"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={styles.button}
+                >
+                  REGISTER
+                </a>
+              </div>
+            </Menu.Item>
+
+            <Menu.Item>
+              <Link
+                className={styles.menuText}
+                href="/theme"
+                style={{ paddingTop: "0" }}
+              >
+                THEME
+              </Link>
+            </Menu.Item>
+            {/* <Menu.Item>
+              <Link className={styles.menuText} href="/speakers">
+                SPEAKERS
+              </Link>
+            </Menu.Item> */}
+            {/* <Menu.Item>
+              <Link className={styles.menuText} href="/erec">
+                EREC
+              </Link>
+            </Menu.Item> */}
+            <Menu.Item>
+              <Link className={styles.menuText} href="/venue">
+                VENUE
+              </Link>
+            </Menu.Item>
+            <Menu.Item>
+              <Link className={styles.menuText} href="/fee">
+                FEE
+              </Link>
+            </Menu.Item>
+            <Menu.Item>
+              <Link className={styles.menuText} href="/faqs">
+                FAQS
+              </Link>
+            </Menu.Item>
+            <Menu.Item>
+              <Link
+                className={styles.menuText}
+                href="/contact"
+                style={{ paddingBottom: "0" }}
+              >
+                CONTACT
+              </Link>
+            </Menu.Item>
+          </div>
+        </Menu.Dropdown>
+      </Menu>
     </>
   );
 }
