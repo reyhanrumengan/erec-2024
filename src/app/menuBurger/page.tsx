5c5ccc5b7c89850daa04047d384b5906e19258f7
Reyhan Rumengan <reyhanrumengan98@gmail.com>
2024-01-01 12:15:44 +0100

    finish mvp

--- src/app/menuBurger/page.tsx
+++ src/app/menuBurger/page.tsx
@@ -3,9 +3,28 @@ import Link from "next/link";
 import styles from "./menuBurger.module.css";
 import { useDisclosure, useClickOutside } from "@mantine/hooks";
 import { Menu, Burger } from "@mantine/core";
+import { useEffect, useState } from "react";
+
+type Route = {
+  path: string;
+  label: string;
+};
 
 export default function MenuBurger() {
   const [burgerOpened, handlers] = useDisclosure();
+  const [currentPathname, setCurrentPathname] = useState<string | undefined>(
+    undefined
+  );
+
+  const routes: Route[] = [
+    { path: "/theme", label: "THEME" },
+    // { path: "/speakers", label: "SPEAKERS" },
+    // { path: "/erec", label: "EREC" },
+    { path: "/venue", label: "VENUE" },
+    { path: "/fee", label: "FEE" },
+    { path: "/faqs", label: "FAQS" },
+    { path: "/contact", label: "CONTACT" },
+  ];
 
   const menuRef = useClickOutside(() => {
     if (burgerOpened) {
@@ -15,6 +34,29 @@ export default function MenuBurger() {
 
   const handleMenuClick = () => {
     handlers.toggle();
+  };
+
+  useEffect(() => {
+    setCurrentPathname(window.location.pathname);
+
+    // Add event listener for client-side navigation changes
+    const handleRouteChange = () => {
+      setCurrentPathname(window.location.pathname);
+    };
+
+    window.addEventListener("popstate", handleRouteChange);
+
+    // Remove the event listener on component unmount
+    return () => {
+      window.removeEventListener("popstate", handleRouteChange);
+    };
+  }, []);
+
+  const isActive = (path: string) => {
+    return path === currentPathname ? styles.activeMenu : "";
+    // return path === currentPathname
+    //   ? `${styles.active} ${styles.activeMenu}`
+    //   : "";
   };
 
   return (
@@ -37,7 +79,7 @@ export default function MenuBurger() {
         </Menu.Target>
 
         <Menu.Dropdown>
-          <div className={styles.dropdownContainer}>
+          <nav className={styles.dropdownContainer}>
             <Menu.Item>
               <div className={styles.buttonContainer}>
                 <a
@@ -52,50 +94,21 @@ export default function MenuBurger() {
               </div>
             </Menu.Item>
 
-            <Menu.Item>
-              <Link
-                className={styles.menuText}
-                href="/theme"
-                style={{ paddingTop: "0" }}
-              >
-                THEME
-              </Link>
-            </Menu.Item>
-            {/* <Menu.Item>
-              <Link className={styles.menuText} href="/speakers">
-                SPEAKERS
-              </Link>
-            </Menu.Item> */}
-            {/* <Menu.Item>
-              <Link className={styles.menuText} href="/erec">
-                EREC
-              </Link>
-            </Menu.Item> */}
-            <Menu.Item>
-              <Link className={styles.menuText} href="/venue">
-                VENUE
-              </Link>
-            </Menu.Item>
-            <Menu.Item>
-              <Link className={styles.menuText} href="/fee">
-                FEE
-              </Link>
-            </Menu.Item>
-            <Menu.Item>
-              <Link className={styles.menuText} href="/faqs">
-                FAQS
-              </Link>
-            </Menu.Item>
-            <Menu.Item>
-              <Link
-                className={styles.menuText}
-                href="/contact"
-                style={{ paddingBottom: "0" }}
-              >
-                CONTACT
-              </Link>
-            </Menu.Item>
-          </div>
+            {routes.map((route) => (
+              <Menu.Item key={route.path}>
+                <Link
+                  href={route.path}
+                  className={`${styles.menuText} ${isActive(route.path)}`}
+                  style={{
+                    paddingTop: route.path === "/theme" ? "0" : undefined,
+                    paddingBottom: route.path === "/contact" ? "0" : undefined,
+                  }}
+                >
+                  {route.label}
+                </Link>
+              </Menu.Item>
+            ))}
+          </nav>
         </Menu.Dropdown>
       </Menu>
     </>
--- src/app/menuSidebar/page.tsx
+++ src/app/menuSidebar/page.tsx
@@ -1,31 +1,66 @@
+"use client";
 import Link from "next/link";
 import styles from "./menuSidebar.module.css";
+import { use, useEffect, useState } from "react";
+
+type Route = {
+  path: string;
+  label: string;
+};
 
 export default function MenuSidebar() {
+  const [currentPathname, setCurrentPathname] = useState<string | undefined>(
+    undefined
+  );
+
+  const routes: Route[] = [
+    { path: "/theme", label: "THEME" },
+    // { path: "/speakers", label: "SPEAKERS" },
+    // { path: "/erec", label: "EREC" },
+    { path: "/venue", label: "VENUE" },
+    { path: "/fee", label: "FEE" },
+    { path: "/faqs", label: "FAQS" },
+    { path: "/contact", label: "CONTACT" },
+  ];
+
+  useEffect(() => {
+    setCurrentPathname(window.location.pathname);
+
+    // Add event listener for client-side navigation changes
+    const handleRouteChange = () => {
+      setCurrentPathname(window.location.pathname);
+    };
+
+    window.addEventListener("popstate", handleRouteChange);
+
+    // Remove the event listener on component unmount
+    return () => {
+      window.removeEventListener("popstate", handleRouteChange);
+    };
+  }, []);
+
+  const isActive = (path: string) => {
+    return path === currentPathname ? styles.activeMenu : "";
+    // return path === currentPathname
+    //   ? `${styles.active} ${styles.activeMenu}`
+    //   : "";
+  };
+
   return (
     <>
       <nav className={styles.menuSidebar}>
-        <Link className={styles.menuText} href="/theme">
-          THEME
-        </Link>
-        {/* <Link className={styles.menuText} href="/speakers">
-          SPEAKERS
-        </Link> */}
-        {/* <Link className={styles.menuText} href="/erec">
-          EREC
-        </Link> */}
-        <Link className={styles.menuText} href="/venue">
-          VENUE
-        </Link>
-        <Link className={styles.menuText} href="/fee">
-          FEE
-        </Link>
-        <Link className={styles.menuText} href="/faqs">
-          FAQS
-        </Link>
-        <Link className={styles.menuText} href="/contact">
-          CONTACT
-        </Link>
+        {routes.map((route) => (
+          <Link
+            key={route.path}
+            href={route.path}
+            // className={`${styles.menuText} ${isActive(route.path)}`}
+            className={styles.menuText}
+            // className={isActive(route.path)}
+          >
+            <div>{route.label}</div>
+            <div className={isActive(route.path)}></div>
+          </Link>
+        ))}
       </nav>
     </>
   );
