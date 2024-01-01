a6a53a48035a61bd0a64bf05323ee3efd6626964
Reyhan Rumengan <reyhanrumengan98@gmail.com>
2024-01-01 02:06:11 +0100

    make header mobile sticky

--- src/app/header/page.tsx
+++ src/app/header/page.tsx
@@ -6,20 +6,22 @@ import MenuBurger from "../menuBurger/page";
 export default function Header() {
   return (
     <>
-      <div className={styles.header}>
-        <div className={styles.headerContainer}>
-          <Link href="/">
-            <Image
-              width={280}
-              height={55}
-              className={styles.headerIcon}
-              alt="European Reformed Evangelical Convention 2024"
-              src="/logo.svg"
-            />
-          </Link>
-        </div>
-        <div className={styles.burgerContainer}>
-          <MenuBurger />
+      <div className={styles.headerMobileContainer}>
+        <div className={styles.header}>
+          <div className={styles.headerContainer}>
+            <Link href="/">
+              <Image
+                width={280}
+                height={55}
+                className={styles.headerIcon}
+                alt="European Reformed Evangelical Convention 2024"
+                src="/logo.svg"
+              />
+            </Link>
+          </div>
+          <div className={styles.burgerContainer}>
+            <MenuBurger />
+          </div>
         </div>
       </div>
     </>
