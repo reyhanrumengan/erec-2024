cfb45366d50051a8b252d7a6ff07b9b543af9832
Reyhan Rumengan <reyhanrumengan98@gmail.com>
2024-01-06 02:30:11 +0100

    wip 80% desktop speakers

--- src/app/speakers/content.tsx
+++ src/app/speakers/content.tsx
@@ -1,8 +1,16 @@
+"use client";
 import Link from "next/link";
 import styles from "./speakers.module.css";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Content() {
+  const [showFullContent, setShowFullContent] = useState(false);
+
+  const handleReadMoreClick = () => {
+    setShowFullContent(!showFullContent);
+  };
+
   return (
     <>
       <div className={styles.heroContainer}>
@@ -17,14 +25,70 @@ export default function Content() {
             </p>
           </div>
         </div>
+
         <div className={styles.container2}>
-          <div></div>
           <div>
-            <p className={styles.text2}>
-              Is this kind of concept right? <br />
-              How do the Reformed tradition and the witness of the Scripture
-              actually teach about this assurance of salvation?
-            </p>
+            <Image
+              width={400}
+              height={533}
+              alt=""
+              src="/dandelion.png"
+              // className={styles.dandelionImage}
+            />
+          </div>
+
+          <div>
+            <div className={styles.arrowIconContainer}>
+              <div>
+                <Image
+                  width={28}
+                  height={28}
+                  className={styles.arrowCircleLeftIcon}
+                  alt="Arrow Circle Right"
+                  src="/arrowCircleRight.svg"
+                />
+              </div>
+
+              <div>
+                <Image
+                  width={28}
+                  height={28}
+                  className={styles.arrowCircleRightIcon}
+                  alt="Arrow Circle Right"
+                  src="/arrowCircleRight.svg"
+                />
+              </div>
+            </div>
+            <div>
+              <div className={styles.speakerName}>
+                Rev. Dr. Billy Kristanto, Ph.D., Th.D.
+              </div>
+              <div
+                className={`${styles.speakerDescription} ${
+                  showFullContent ? styles.full : ""
+                }`}
+              >
+                Billy Kristanto (Dr. phil., Dr. theol., Universität Heidelberg)
+                is lecturer of systematic theology at International Reformed
+                Evangelical Seminary Jakarta and member of theological
+                commission of World Reformed Fellowship. For 24 years, he serves
+                as pastor of International Reformed Evangelical Church. He is
+                the author of Ajarlah Kami Bertumbuh and Sola Dei Gloria. His
+                research interests include topics in soteriology, anthropology,
+                spirituality, the theology of J. Calvin, and J. S. Bach. His
+                sermons can be found in the youtube channel Sola Dei Gloria.
+              </div>
+
+              {showFullContent || (
+                <div
+                  onClick={handleReadMoreClick}
+                  className={styles.readMoreButton}
+                  style={{ cursor: "pointer" }}
+                >
+                  READ MORE
+                </div>
+              )}
+            </div>
           </div>
 
           {/* change link later to redirect to Speaker Page */}
