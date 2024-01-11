b8d27a318e91cb7ac8f1d44a659c9b2d38add9da
Reyhan Rumengan <reyhanrumengan98@gmail.com>
2024-01-11 23:34:52 +0100

    set up layout schedule

--- src/app/schedule/content.tsx
+++ src/app/schedule/content.tsx
@@ -15,61 +15,67 @@ export default function Content() {
   return (
     <>
       <div className={styles.heroContainer}>
-        <div className={styles.container1}>
-          <div>
-            <p className={styles.pageHeader}>SCHEDULE</p>
-          </div>
-          <div>
-            <p className={styles.smallText}>
-              Schedule is still being finalized. A detailed and downloadable
-              schedule will be available a few weeks before the event date.
-            </p>
-          </div>
-        </div>
-
-        {/* Schedule Box */}
-        <div className={styles.container2}>
-          <div></div>
-
-          <div className={styles.arrowIconContainer}>
+        <div className={styles.content}>
+          <div className={styles.container1}>
             <div>
-              <Image
-                width={28}
-                height={28}
-                className={styles.arrowCircleLeftIcon}
-                alt="Arrow Circle Right"
-                src="/arrowCircleRight.svg"
-                onClick={() => setChangeContent(!changeContent)}
-              />
+              <p className={styles.pageHeader}>SCHEDULE</p>
             </div>
-
             <div>
-              <Image
-                width={28}
-                height={28}
-                className={styles.arrowCircleRightIcon}
-                alt="Arrow Circle Right"
-                src="/arrowCircleRight.svg"
-                onClick={() => setChangeContent(!changeContent)}
-              />
+              <p className={styles.smallText}>
+                Schedule is still being finalized. A detailed and downloadable
+                schedule will be available a few weeks before the event date.
+              </p>
             </div>
           </div>
-        </div>
 
-        {/* Registration Button */}
-        <div className={styles.container3}>
-          {/* <Link href="/erec" className={styles.linkToSpeakerContainer}>
-            <p className={styles.linkToRegister}>
-              Limited places available. Register today.
-            </p>
-            <Image
-              width={28}
-              height={28}
-              className={styles.arrowCircleRightIcon}
-              alt="Arrow Circle Right"
-              src="/arrowCircleRight.svg"
-            />
-          </Link> */}
+          {/* Schedule Box */}
+          <div className={styles.container2}>
+            <div>asdasdsa</div>
+
+            <div className={styles.arrowIconContainer}>
+              <div>
+                <Image
+                  width={28}
+                  height={28}
+                  className={styles.arrowCircleLeftIcon}
+                  alt="Arrow Circle Right"
+                  src="/arrowCircleRight.svg"
+                  onClick={() => setChangeContent(!changeContent)}
+                />
+              </div>
+
+              <div>
+                <Image
+                  width={28}
+                  height={28}
+                  className={styles.arrowCircleRightIcon}
+                  alt="Arrow Circle Right"
+                  src="/arrowCircleRight.svg"
+                  onClick={() => setChangeContent(!changeContent)}
+                />
+              </div>
+            </div>
+          </div>
+
+          {/* Registration Button */}
+          <div className={styles.linkContainer}>
+            <div>
+              <p className={styles.text}>Limited places available.</p>
+              <p className={styles.text}>Register today.</p>
+            </div>
+
+            <div className={styles.buttonContainer}>
+              <a
+                // href="https://link.grii.eu/EREC2024"
+                href="https://forms.gle/5nBNSFBUs6AUr8bz6"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styles.button}
+              >
+                REGISTER
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </>
